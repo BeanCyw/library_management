c62d97bbfe51108832ba0f8af24607301aac0b95
BeanCyw <62070046@it.kmitl.ac.th>
2021-02-18 23:44:40 +0700

    First Commit

---
+++ library_management/library_management/doctype/library_setiings/library_setiings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, it046 and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Library Setiings', {
+	// refresh: function(frm) {
+
+	// }
+});
